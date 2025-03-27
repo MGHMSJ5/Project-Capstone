1e508c993603dd5ea2c630c8022ce4437297c920
MG_HM_SJ5 <elise.koezema@hotmail.com>
2025-03-27 14:44:53 +0100

    Added drag to player movement
    feat(player movement): movement drag

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,12 @@ public class PlayerController : MonoBehaviour
     [Header("Movement")]
     [SerializeField] private Transform _orientation; 
     [SerializeField] private float _speed;
+    [SerializeField] private float _groundDrag;
+
+    [Header("Ground Check")]
+    [SerializeField] private float _playerHeight;
+    [SerializeField] private LayerMask _groundMask;
+    private bool _grounded;
 
     private float horizontalInput;
     private float verticalInput;
@@ -20,7 +26,21 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        //ground check
+        _grounded = Physics.Raycast(transform.position, Vector3.down, _playerHeight * 0.5f + 0.2f, _groundMask);
+
         PlayerInput();
+        SpeedControl();
+
+        //handle drag
+        if (_grounded)
+        {
+            _rb.drag = _groundDrag;
+        }
+        else
+        {
+            _rb.drag = 0;
+        }
     }
 
     void FixedUpdate()
@@ -40,5 +60,17 @@ public class PlayerController : MonoBehaviour
         _direction = _orientation.forward * verticalInput + _orientation.right * horizontalInput;
         _rb.MovePosition(_rb.position + _direction.normalized * _speed * Time.fixedDeltaTime);
         //_rb.velocity = _direction.normalized * _speed;
+    }
+
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(_rb.velocity.x, 0f, _rb.velocity.z);
+
+        //limit velocuty if needed
+        if (flatVel.magnitude > _speed)
+        {
+            Vector3 limitedVel = flatVel.normalized * _speed;
+            _rb.velocity = new Vector3(limitedVel.x, _rb.velocity.y, limitedVel.z);
+        }
     }
 }
