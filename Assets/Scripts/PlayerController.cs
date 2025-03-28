ee2d95d1e19cc4990bfd15e9e13f6689763752d0
MG_HM_SJ5 <elise.koezema@hotmail.com>
2025-03-28 11:47:40 +0100

    Jumping
    feat(player movement): player jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,12 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float _speed;
     [SerializeField] private float _groundDrag;
 
+    [Header("Jumping")]
+    [SerializeField] private float _jumpForce;
+    [SerializeField] private float _jumpCooldown;
+    [SerializeField] private float _airMultiplier;
+    private bool _readyToJump = true;
+
     [Header("Ground Check")]
     [SerializeField] private float _playerHeight;
     [SerializeField] private LayerMask _groundMask;
@@ -52,14 +58,47 @@ public class PlayerController : MonoBehaviour
     {
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetButton("Jump") && _readyToJump && _grounded)
+        {
+            _readyToJump = false;
+            Jump();
+            Invoke("ResetJump", _jumpCooldown);
+        }
     }
 
     private void MovePlayer()
     {
         //calculate movement direction
         _direction = _orientation.forward * verticalInput + _orientation.right * horizontalInput;
-        _rb.MovePosition(_rb.position + _direction.normalized * _speed * Time.fixedDeltaTime);
-        //_rb.velocity = _direction.normalized * _speed;
+
+        if (_grounded)
+        {
+            _rb.MovePosition(_rb.position + _direction.normalized * _speed * Time.fixedDeltaTime);
+            //_rb.velocity = _direction.normalized * _speed;
+        }
+        else if (!_grounded) //in the air
+        {
+            _rb.MovePosition(_rb.position + _direction.normalized * _speed * _airMultiplier * Time.fixedDeltaTime);
+        }
+
+    }
+
+    private void Jump()
+    {
+        //reset y velocity (to make sure that player always jumps the same height
+        if (_rb.velocity.y < 0f)
+        {
+            _rb.velocity = new Vector3(_rb.velocity.x, 0f, _rb.velocity.z);
+        }
+
+        //use impulse, because the force is only applied once
+        _rb.AddForce(transform.up * _jumpForce, ForceMode.Impulse);
+    }
+
+    private void ResetJump()
+    {
+        _readyToJump = true;
     }
 
     private void SpeedControl()
