a99165af0fa77aee94d76c20c65c626d3b4a9a8b
MGHMSJ5 <100706224+MGHMSJ5@users.noreply.github.com>
2025-04-09 10:19:04 +0200

    Suggested change on code comments

--- Assets/Scripts/Interaction/NPCInteractEXAMPLE.cs
+++ Assets/Scripts/Interaction/NPCInteractEXAMPLE.cs
@@ -17,16 +17,17 @@ public class NPCInteractEXAMPLE : BaseInteract
     protected override void Update()
     {
         base.Update();
-        // Check for if the dialogue disappeared after appearing
-        if (!_dialogueObject.activeInHierarchy && _dialogueVisible)
+        // Check if the dialogue disappeared after appearing
+        if (!_dialogueObject.activeInHierarchy && _dialogueVisible) //Nick: How does this even happen at the same time? Isn't the first statement false and the second true? How does that not lead to an error? Maybe just me not understanding the code.
         {
             _dialogueVisible = false;
-            // To interact again while still in the collider:
+            // To interact again while still in the collider
             SetInteract(true);
             // Enable the player movement
             _playerController.enabled = true;
         }
     }
+
     protected override void InteractFunction()
     {
         base.InteractFunction();
@@ -35,6 +36,6 @@ public class NPCInteractEXAMPLE : BaseInteract
         _dialogueObject.SetActive(true);
         _dialogueVisible = true;
         // Disable the player movement
-        _playerController.enabled = false;
+        _playerController.enabled = false; //Nick: I don't think this is necessarily needed. I think it would be nice to let the player just move away if they wanted to and the dialogue then closes if they move outside a specific range. Especially if we do the dialogue with text bubbles. If we do dialogue with textboxes like in animal crossing then this is handy. Something I will need to design and then we discuss with the team.
     }
 }
