f24bd15414d8f2c197a46f48df43eae06b2a85ac
MG_HM_SJ5 <elise.koezema@hotmail.com>
2025-04-09 11:21:55 +0200

    Feedback Iteraction
    refactor(NPC interact): removed the weird lines and fixed the dialogie to dialogue

--- Assets/Scripts/Interaction/NPCInteractEXAMPLE.cs
+++ Assets/Scripts/Interaction/NPCInteractEXAMPLE.cs
@@ -34,7 +34,6 @@ public class NPCInteractEXAMPLE : BaseInteract
     protected override void Update()
     {
         base.Update();
-<<<<<<< Updated upstream
         // Based on the dialogueType set, the type of dialogue disappearing will change
         // Option 1: Player movement is disabled, remove the dialogue box after pressing space. Player can interact with dialogue again without having to exit and enter the trigger
         // Option 2: Player movement is not disabled. The dialogue box will be removed when the player is past a certain distance from the NPC
@@ -44,7 +43,7 @@ public class NPCInteractEXAMPLE : BaseInteract
         }
         else
         {
-            DialogieOption2();
+            DialogueOption2();
         }
     }
 
@@ -72,15 +71,14 @@ public class NPCInteractEXAMPLE : BaseInteract
             _dialogueObject.SetActive(false);
         }
         // Check if the dialogue disappeared after appearing
-        //Nick: How does this even happen at the same time? Isn't the first statement false and the second true? How does that not lead to an error? Maybe just me not understanding the code.
+        //Nick: How does this even happen at the same time? Isn't the first statement false and the second true?
+        //Nick: How does that not lead to an error? Maybe just me not understanding the code.
         // Elise: This part is to make it so that when the dialogue has disappeared after interacting with it, the player can interact with it again (without having to exit and enter the trigger)
         // Elise: To do that, in the InteractFunction, it sets the _dialogueVisible bool to true, so that it knows that the dialogue has appeared/is active.
         // Elise: Then in this if statement below, it checks for if the dialogue has disappeared 'after' it was active. 
         if (!_dialogueObject.activeInHierarchy && _dialogueVisible)
-=======
         // Check if the dialogue disappeared after appearing
         if (!_dialogueObject.activeInHierarchy && _dialogueVisible) //Nick: How does this even happen at the same time? Isn't the first statement false and the second true? How does that not lead to an error? Maybe just me not understanding the code.
->>>>>>> Stashed changes
         {
             _dialogueVisible = false;
             // To interact again while still in the collider
@@ -90,8 +88,7 @@ public class NPCInteractEXAMPLE : BaseInteract
         }
     }
 
-<<<<<<< Updated upstream
-    private void DialogieOption2()
+    private void DialogueOption2()
     {
         // Dialogue will disappear when the player is outside of a specific range (_disableDistance)
         float distance = Vector3.Distance(transform.position, _player.transform.position);
@@ -99,16 +96,5 @@ public class NPCInteractEXAMPLE : BaseInteract
         {
             _dialogueObject.SetActive(false);
         }
-=======
-    protected override void InteractFunction()
-    {
-        base.InteractFunction();
-        print("NPC interaction (repeatable)");
-        // Enable dialogue object
-        _dialogueObject.SetActive(true);
-        _dialogueVisible = true;
-        // Disable the player movement
-        _playerController.enabled = false; //Nick: I don't think this is necessarily needed. I think it would be nice to let the player just move away if they wanted to and the dialogue then closes if they move outside a specific range. Especially if we do the dialogue with text bubbles. If we do dialogue with textboxes like in animal crossing then this is handy. Something I will need to design and then we discuss with the team.
->>>>>>> Stashed changes
     }
 }
