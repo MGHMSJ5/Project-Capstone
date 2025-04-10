314915de8f344dfbfdbbb0ab646d3ab29fca23d5
MG_HM_SJ5 <elise.koezema@hotmail.com>
2025-04-10 14:49:45 +0200

    Carrying with new layout
    fix(script): changed the script to work with the new layout
    refactor(script): changed the layout of the script a bit for better reading

--- Assets/Scripts/Interaction/CarryObjectEXAMPLE.cs
+++ Assets/Scripts/Interaction/CarryObjectEXAMPLE.cs
@@ -3,40 +3,73 @@ using UnityEngine;
 
 public class CarryObjectEXAMPLE : BaseInteract
 {
-    [SerializeField]
-    private Collider _objectCollider; // Set in the prefab
+    private Transform _parent;
     private Transform _playerCarryPoint;
     private bool _isCarrying = false;
+
+    private Collider _parentCollider;
+    private GravityBody _gravityBody;
 
     protected override void Start()
     {
         base.Start();
+        _parent = transform.parent;
         _playerCarryPoint = GameObject.Find("CarryPoint").GetComponent<Transform>();
+
+        _parentCollider = _parent.GetComponent<Collider>();
+        _gravityBody = _parent.GetComponent<GravityBody>();
     }
 
+    protected override void Update()
+    {
+        base.Update();
+        if (_isCarrying)
+        {
+            // Reset the position of the parent when it is not (0,0,0)
+            // Because it first checks if it's not Vector3.zero, it reduces the unnecessary amount of times it reset the position
+            if (_parent.localPosition != Vector3.zero)
+            {
+                _parent.localPosition = Vector3.zero;
+            }
+        }
+    }
+    // Interacting will happen when the player is either carring the object or not
     protected override void InteractFunction()
     {
         base.InteractFunction();
         if (!_isCarrying)
         {
-            print("pick up");
-            // Set parent to the carry point and reset the position so that the object will always be on the same position as the carry point
-            transform.SetParent(_playerCarryPoint);
-            transform.localPosition = Vector3.zero;
-            // Set collider to trigger so that it won't collider with anything
-            _objectCollider.isTrigger = true;
+            Carry();
         }
         else
         {
-            print("let go");
-            // Reset the parent
-            transform.parent = null;
-            // Disable trigger function so that it can collide again
-            _objectCollider.isTrigger = false;
+            LetGo();
         }
         
         // To interact again while still in the collider:
         SetInteract(true);
+        // Change bool 
         _isCarrying = !_isCarrying;
+    }
+
+    private void Carry()
+    {
+        // Set parent to the carry point so that the parent will follow the player's carry point
+        _parent.SetParent(_playerCarryPoint);
+
+        // Set collider's Trigger on so that it won't collider with anything
+        _parentCollider.isTrigger = true;
+        // Also disable the gravity script so that it wont fall to the floow
+        _gravityBody.enabled = false;
+    }
+
+    private void LetGo()
+    {
+        // Reset the parent's parent
+        _parent.parent = null;
+        // Disable trigger function so that it can collide again
+        _parentCollider.isTrigger = false;
+        // Enable the gravity so that it falls to the planet
+        _gravityBody.enabled = true;
     }
 }
