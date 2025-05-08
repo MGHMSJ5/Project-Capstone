63bdac1b2d49551ceb1c53a05e9540bb371aeec8
MG_HM_SJ5 <elise.koezema@hotmail.com>
2025-05-08 11:45:54 +0200

    Removed the change

--- Assets/Scripts/QuestSystem/QuestStep.cs
+++ Assets/Scripts/QuestSystem/QuestStep.cs
@@ -5,18 +5,11 @@ using UnityEngine;
 //This class is supposed to be inherited by another script and not used alone, therefore it is an abstract class.
 public abstract class QuestStep : MonoBehaviour
 {
-    [Header("Quest")]
-    [SerializeField]
-    private QuestInfoSO questInfo;
     [SerializeField] public string description;
 
     private bool isFinished = false;
     private string questId;
 
-    private void Awake()
-    {
-        questId = questInfo.id;
-    }
     public void InitializeQuestStep(string questId)
     {
         this.questId = questId;
