7df0a3778aafa0fde9208e6dad1330f3a608e1db
Nick <niklasrissedrums@gmail.com>
2025-05-08 11:06:36 +0200

    Trying to make quests work

--- Assets/Scripts/GameEventsManager.cs
+++ Assets/Scripts/GameEventsManager.cs
@@ -8,7 +8,7 @@ public class GameEventsManager : MonoBehaviour
    public static GameEventsManager instance { get; private set; }
 
     public QuestEvents questEvents;
-    public BaseInteract baseInteract;
+    //public BaseInteract baseInteract;
     //public RewardEvents rewardEvents;
 
    private void Awake()
@@ -21,7 +21,7 @@ public class GameEventsManager : MonoBehaviour
 
         //initialise all events
         questEvents = new QuestEvents();
-        baseInteract = new BaseInteract();
+        //baseInteract = new BaseInteract();
         //rewardEvents = new RewardEvents();
    }
 }
