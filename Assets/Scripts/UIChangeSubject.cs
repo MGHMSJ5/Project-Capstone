432b5c00892da5cd51c039fe77982ce53d2cdb58
MG_HM_SJ5 <elise.koezema@hotmail.com>
2025-05-14 11:05:31 +0200

    Controller detection
    feat(script): added code so that it now can detect whether a controller is connected or disconeccted

--- Assets/Scripts/UIChangeSubject.cs
+++ Assets/Scripts/UIChangeSubject.cs
@@ -1,41 +1,61 @@
 using System;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class UIChangeSubject : MonoBehaviour
 {
-    private bool connected = false;
-    public event Action SwitchUI;
+    [Tooltip("Action that needs to be subscribed to with a function if the UI needs to be changed.")]
+    public event Action<bool> UISwitch;
 
-    void Awake()
+    private Coroutine _connectCheckCoroutine;
+
+    private void OnEnable()
     {
-        StartCoroutine(CheckForControllers());
-    }
-    private void InputHasSwitched()
-    {
-        SwitchUI?.Invoke();
+        InputSystem.onDeviceChange += OnDeviceChange;
     }
 
-
-    IEnumerator CheckForControllers()
+    private void OnDisable()
     {
-        while (true)
+        InputSystem.onDeviceChange -= OnDeviceChange;
+    }
+
+    private void InputChanged(bool inputController)
+    {
+        UISwitch?.Invoke(inputController);
+    }
+
+    // Function that is used to check if a controller has been conntected or disconnected
+    private void OnDeviceChange(InputDevice device, InputDeviceChange change)
+    {
+        // Check if the device is a GamePad
+        if (device is Gamepad)
+        {   // Handle different types of device changes
+            switch (change)
+            {
+                case InputDeviceChange.Added:
+                    if (_connectCheckCoroutine != null) StopCoroutine(_connectCheckCoroutine);
+                    _connectCheckCoroutine = StartCoroutine(ConfigmControllerConnection(true));
+                    break;
+                case InputDeviceChange.Removed:
+                    if (_connectCheckCoroutine != null) StopCoroutine(_connectCheckCoroutine);
+                    _connectCheckCoroutine = StartCoroutine(ConfigmControllerConnection(false));
+                    break;
+            }
+        }
+    }
+
+    private IEnumerator ConfigmControllerConnection(bool connecting)
+    {
+        yield return new WaitForSeconds(1f);
+
+        if (connecting && Gamepad.current != null)
         {
-            var controllers = Input.GetJoystickNames();
-
-            if (!connected && controllers.Length > 0)
-            {
-                connected = true;
-                Debug.Log("Connected");
-
-            }
-            else if (connected && controllers.Length == 0)
-            {
-                connected = false;
-                Debug.Log("Disconnected");
-            }
-
-            yield return new WaitForSeconds(1f);
+            InputChanged(true);
+        }
+        else if (!connecting && Gamepad.current == null)
+        {
+            InputChanged(false);
         }
     }
 }
