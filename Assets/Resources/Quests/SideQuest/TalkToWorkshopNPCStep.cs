47637416f2dc819bdf3ca97fcfd4fd9b3688de3b
Nick <niklasrissedrums@gmail.com>
2025-05-19 13:14:48 +0200

    QuestStep improvements

--- Assets/Resources/Quests/SideQuest/TalkToWorkshopNPCStep.cs
+++ Assets/Resources/Quests/SideQuest/TalkToWorkshopNPCStep.cs
@@ -1,8 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class TalkToWorkshopNPCStep : QuestStep
 {
-    //Add override for the interaction/dialogue to advance the quest step
+    private void OnEnable()
+    {
+        GameObject.Find("Neval").GetComponent<BaseInteract>().onSubmitPressed += GrandAbility;
+    }
+
+    private void OnDisable()
+    {
+        GameObject.Find("Neval").GetComponent<BaseInteract>().onSubmitPressed -= GrandAbility;
+    }
+
+    //Add that the queststep is finished when talking to NPC
+    private void GrandAbility()
+    {
+        FinishQuestStep();
+        GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHover>()._hoverAbilityGranted = true;
+    }
+
 }
