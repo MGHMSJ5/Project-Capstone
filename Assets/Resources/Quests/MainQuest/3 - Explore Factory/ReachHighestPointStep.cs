b18ca0647424797f119154cfdca220f83a0ce4fb
Nick <niklasrissedrums@gmail.com>
2025-05-19 12:25:22 +0200

    More fixes

--- Assets/Resources/Quests/MainQuest/3 - Explore Factory/ReachHighestPointStep.cs
+++ Assets/Resources/Quests/MainQuest/3 - Explore Factory/ReachHighestPointStep.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 [RequireComponent(typeof(BoxCollider))]
 public class ReachHighestPointStep : QuestStep
 {
+
+    private void OnEnable()
+    {
+        GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerPulse>().hasPulseAbility = true;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Player"))
