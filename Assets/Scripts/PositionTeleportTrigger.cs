f1d415bb1cb0005e49fb27e5e0c244ebbbbd8663
MG_HM_SJ5 <elise.koezema@hotmail.com>
2025-05-20 14:27:10 +0200

    New Position Teleportation is in
    feat(position change): finished the script that changes the position of the player. Fixed some issues on the way.

--- Assets/Scripts/PositionTeleportTrigger.cs
+++ Assets/Scripts/PositionTeleportTrigger.cs
@@ -10,37 +10,55 @@ public class PositionTeleportTrigger : MonoBehaviour
     [Tooltip("Set to true if the NEXT and PREVIOUS teleport location is the Factory.")]
     [SerializeField]
     private bool _changeCameraWhenTeleport = false;
+    // Made bool to check if the player has entered, because it was causing some issues withouts
+    private bool _hasEntered = false;
     private CanvasSceneTransition _canvasSceneTransition;
 
     private GameObject _mainCamera;
     private GameObject _factoryCamera;
     private Transform _playerTransform;
+    private PlayerController _playerController;
 
     void Awake()
     {
-        _mainCamera = GameObject.Find("Main Camera");
+        _mainCamera = GameObject.Find("Main_Camera");
         _factoryCamera = GameObject.Find("FactoryCamera");
         _canvasSceneTransition = GameObject.Find("Canvas_SceneTransition").GetComponent<CanvasSceneTransition>();
         _playerTransform = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        _playerController = _playerTransform.GetComponent<PlayerController>();
     }
     private void Start()
     {
+        // Deactivate the factory camera at the start
         _factoryCamera.SetActive(false);
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        _canvasSceneTransition.FadeAction += ChangeCamera;
-        _canvasSceneTransition.CanvasFadeInAndOut(2f);
+        // If the player has entered, and has not entered yet
+        if (other.CompareTag("Player") && !_hasEntered)
+        {
+            _hasEntered = true; // Set to true so that this code won't be called again (to prevent issue)
+            Invoke("EnablePlayerMovement", 0.5f); // Invoke this 0.5 seconds later to stop the player movement
+            _canvasSceneTransition.FadeAction += ChangeCamera; // Subscribe to FadeAction event
+            _canvasSceneTransition.CanvasFadeInAndOut(2f); // Call the fading
+        }
     }
 
     private void ChangeCamera()
-    {
-        _playerTransform.localPosition = _teleportLocation.position;
+    {   // Function that disables and enables the camera's if needed + changes the player's position
         if (_changeCameraWhenTeleport)
         {
-            _mainCamera.SetActive(!_mainCamera.activeInHierarchy);
-            _factoryCamera.SetActive(!_factoryCamera.activeInHierarchy);
+            print(_mainCamera.activeSelf);
+            _mainCamera.SetActive(!_mainCamera.activeSelf);
+            _factoryCamera.SetActive(!_factoryCamera.activeSelf);
         }
+        _playerTransform.localPosition = _teleportLocation.position;
+        _playerController.enabled = true;
+    }
+    private void EnablePlayerMovement()
+    {
+        _playerController.enabled = false;
+        _hasEntered = false;
     }
 }
