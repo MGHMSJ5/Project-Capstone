6c4d80a3b024354c74cc9342319e4f02828c1e5f
Mihai-Iulian Paduretu <sirtangogibus@gmail.com>
2025-05-26 13:43:05 +0200

    Dialogue Manager/Pause Manager fix
    fix: Pause Menu no longer opens when dialogue active.

--- Assets/Scripts/UI/PauseMenuManager.cs
+++ Assets/Scripts/UI/PauseMenuManager.cs
@@ -8,6 +8,7 @@ public class PauseMenuManager : MonoBehaviour
     [Header("Main UI")]
     public GameObject pauseMenuUI;
     public GameObject firstPauseMenuButton;
+    public GameObject dialoguePanel;
 
     [Header("Load Save UI")]
     public GameObject loadSavePanel;
@@ -40,26 +41,29 @@ public class PauseMenuManager : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetButtonDown("Pause"))
-        {
-            if (isPaused)
-                Resume();
-            else
-                Pause();
-        }
+    if (Input.GetButtonDown("Pause"))
+    {
+        if (dialoguePanel != null && dialoguePanel.activeSelf)
+            return; // Can't pause if dialogue is active!
 
-        if (Input.GetAxis("Mouse X") != 0 || Input.GetAxis("Mouse Y") != 0)
-        {
-            lastInputMethod = "Mouse";
-        }
+        if (isPaused)
+            Resume();
+        else
+            Pause();
+    }
 
-        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+    if (Input.GetAxis("Mouse X") != 0 || Input.GetAxis("Mouse Y") != 0)
+    {
+        lastInputMethod = "Mouse";
+    }
+
+    if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+    {
+        if (lastInputMethod != "Controller")
         {
-            if (lastInputMethod != "Controller")
-            {
-                RestoreControllerFocus();
-                lastInputMethod = "Controller";
-            }
+            RestoreControllerFocus();
+            lastInputMethod = "Controller";
+        }
         }
     }
 
