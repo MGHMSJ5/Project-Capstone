88b2744bc5dc3ec16edf08c0fa0829e9a5a3a02a
MG_HM_SJ5 <elise.koezema@hotmail.com>
2025-05-27 16:23:25 +0200

    Changed brigde
    refactor(moved code)

---
+++ Assets/Scripts/Repair/RepairBridge.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepairBridge : MonoBehaviour
+{
+    private BaseInteract baseInteract;
+    private CanvasSceneTransition _canvasSceneTransition;
+
+    [SerializeField] private Vector3 finalPosition;
+    [SerializeField] private Quaternion finalRotation;
+
+    private void Awake()
+    {
+        baseInteract = GameObject.Find("LandingAreaBridge").GetComponent<BaseInteract>();
+        _canvasSceneTransition = GameObject.Find("Canvas_SceneTransition").GetComponent<CanvasSceneTransition>();
+    }
+    private void OnEnable()
+    {
+        baseInteract.onSubmitPressed += FixBridge;
+    }
+
+    private void OnDisable()
+    {
+        baseInteract.onSubmitPressed -= FixBridge;
+    }
+
+    //Add that the queststep is finished when interacting with the bridge
+    private void FixBridge()
+    {
+        _canvasSceneTransition.FadeAction += BridgeChange;
+        _canvasSceneTransition.FadeAction += BridgeChange;
+        _canvasSceneTransition.CanvasFadeInAndOut(2f);
+
+        //FinishQuestStep();
+        baseInteract.onSubmitPressed -= FixBridge;
+
+        baseInteract.InvokeSubmitPressed();
+    }
+
+    private void BridgeChange()
+    {
+        GameObject.Find("LandingAreaBridge").transform.position = finalPosition;
+        GameObject.Find("LandingAreaBridge").transform.rotation = finalRotation;
+    }
+}
