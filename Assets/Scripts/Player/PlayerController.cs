83a7e03fe065ea055be480db531a13bbcca50550
MG_HM_SJ5 <elise.koezema@hotmail.com>
2025-06-03 09:40:50 +0200

    Changed the speed again
    refactor(player speed): the player jump force now gets calculated by (_speed + _sprintSpeed) / 2 instead of _speed or _sprintSpeed;

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -171,7 +171,7 @@ public class PlayerController : MonoBehaviour
         }
         else if (!_grounded) //in the air
         {
-            _rb.MovePosition(_rb.position + _direction.normalized * _sprintSpeed * _airMultiplier * Time.fixedDeltaTime);
+            _rb.MovePosition(_rb.position + _direction.normalized * ((_speed + _sprintSpeed)/2) * _airMultiplier * Time.fixedDeltaTime);
         }
         // Elise: Could you write a comment that would explain how this works?
         // Thomas: Check the velocity of the player to change the Speed variable of the animator to change the animation
