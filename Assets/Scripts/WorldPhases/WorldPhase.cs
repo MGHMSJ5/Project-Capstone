88fae215494805a9600d74465dceded1f22715f0
MG_HM_SJ5 <elise.koezema@hotmail.com>
2026-08-24 17:58:30 +0200

    Some starter scripts
    Still figuring out on how to handle the transition from cold to warm

---
+++ Assets/Scripts/WorldPhases/WorldPhase.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "WorldPhase", menuName = "ScriptableObjects/WorldPhase")]
+public class WorldPhase : ScriptableObject
+{
+    public string phaseName;
+}
